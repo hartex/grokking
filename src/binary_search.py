d9b5d4b5cb7d46cba56a7abec3ba46f2f7763536
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2018-09-03 19:32:32 +0300

    binary search

--- src/binary_search.py
+++ src/binary_search.py
@@ -1,15 +1,20 @@
-def binary_search(list, item):
-    length = len(list)
-    if length == 0:
-        return None
+def binary_search(list_to_search, item):
+    def inner(list_to_search, item, start, end):
+        if end == 0:
+            return None
 
-    middleIndex = (length - 1) // 2
-    if list[middleIndex] == item:
-        return middleIndex
-    elif list[middleIndex] < item:
-        return binary_search(list[:middleIndex], item)
-    elif list[middleIndex] > item:
-        return binary_search(list[middleIndex:], item)
+        middle_index = end // 2
+        middle_item = list_to_search[middle_index]
+        if middle_item == item:
+            return middle_index
+        elif item < middle_item:
+            end = middle_index
+            return inner(list_to_search, item, start, end)
+        else:
+            start = middle_index
+            return inner(list_to_search, item, start, end)
+
+    return inner(list_to_search, item, 0, len(list_to_search))
 
 
-print(binary_search([2, 4, 1, 5, 6, 8], 8))
+print(binary_search([1, 2, 4, 5, 6, 8, 10], 8))
