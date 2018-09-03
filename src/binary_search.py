5e2e78b9492dd97ad68acf968dfbb7b6d3899e76
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2018-09-04 00:11:10 +0300

    finally finished binary search

--- src/binary_search.py
+++ src/binary_search.py
@@ -1,20 +1,18 @@
 def binary_search(list_to_search, item):
     def inner(list_to_search, item, start, end):
-        if end == 0:
+        if start > end:
             return None
 
-        middle_index = end // 2
+        middle_index = (end + start) // 2
         middle_item = list_to_search[middle_index]
         if middle_item == item:
             return middle_index
         elif item < middle_item:
-            end = middle_index
-            return inner(list_to_search, item, start, end)
+            return inner(list_to_search, item, start, middle_index - 1)
         else:
-            start = middle_index
-            return inner(list_to_search, item, start, end)
+            return inner(list_to_search, item, middle_index + 1, end)
 
     return inner(list_to_search, item, 0, len(list_to_search))
 
 
-print(binary_search([1, 2, 4, 5, 6, 8, 10], 8))
+print(binary_search([1, 2, 4, 5, 6, 8, 10], 10))
