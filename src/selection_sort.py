3ddf8f12c67e86a04c75bd6e0fb61fcbf1b93137
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2018-09-05 19:56:27 +0300

    some small refactoring

--- src/selection_sort.py
+++ src/selection_sort.py
@@ -10,25 +10,23 @@ def find_smallest(arr, start=0):
 
 
 def selection_sort(list_to_sort, in_place=False):
-    if in_place == True:
+    if in_place:
         return selection_sort_in_place(list_to_sort)
     else:
         return selection_sort_new(list_to_sort)
 
+
 # sorting in place
-
-
 def selection_sort_in_place(list_to_sort):
-    for i in range(0, len(list_to_sort)):
+    for i in range(len(list_to_sort)):
         smallest_index = find_smallest(list_to_sort, i)
         current_element = list_to_sort[i]
         list_to_sort[i] = list_to_sort[smallest_index]
         list_to_sort[smallest_index] = current_element
     return list_to_sort
 
+
 # sorting and returning new array
-
-
 def selection_sort_new(list_to_sort):
     result = []
     for i in range(len(list_to_sort)):
