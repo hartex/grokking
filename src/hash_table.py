ec52a0cbd09a1ea10cdfac5ab3e02856cd853a6c
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2018-10-02 09:42:13 +0300

    start working on hash table

--- src/hash_table.py
+++ src/hash_table.py
@@ -1,3 +1,16 @@
 class HashTable:
-    def __init__(self, load_factor):
-        self.a = 0
+    def __init__(self, load_factor=0.7, initial_capacity=20):
+        self.load_factor = load_factor
+        self.inner_table = [None] * initial_capacity
+        self.threshold = load_factor * initial_capacity
+        self.size = 0
+
+    def get(self, key):
+        return 1
+
+    def set(self, key, value):
+        # get hash
+        return value
+
+    def delete(self, key):
+        return key
--- src/merge_sort.py
+++
@@ -1,5 +0,0 @@
-def merge_sort(array):
-    return array
-
-
-print(merge_sort([6, 6, 1, 5, 2, 7, 3, 9, 4]))
