4737ce8747a0741f6defd42a5dbc690071b7af27
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2018-10-08 09:55:11 +0300

    start working on hash table

--- src/hash_table.py
+++ src/hash_table.py
@@ -5,12 +5,42 @@ class HashTable:
         self.threshold = load_factor * initial_capacity
         self.size = 0
 
+    def hash_function(self, key_str):
+        return sum([ord(c) for c in key_str]) % len(self.inner_table)
+
     def get(self, key):
-        return 1
+        index = self.hash_function(key)
+        array_under_index = self.inner_table[index]
+        if array_under_index:
+            for i in array_under_index:
+                if i[0] == key:
+                    return i[1]
+        return None
 
     def set(self, key, value):
-        # get hash
-        return value
+        index = self.hash_function(key)
+        array_under_index = self.inner_table[index]
+        if array_under_index:
+            array_under_index.push((key, value))
+        else:
+            self.inner_table[index] = [(key, value)]
+        self.size += 1
 
     def delete(self, key):
-        return key
+        index = self.hash_function(key)
+        array_under_index = self.inner_table[index]
+        if array_under_index:
+            for i in array_under_index:
+                if i[0] == key:
+                    array_under_index[i] = None
+                    break
+
+
+instance = HashTable()
+instance.set('string', 1000)
+instance.set('strin123g', 1000)
+instance.set('stri123344ng', 1000)
+instance.delete('string')
+instance.set('stri1244ng', 1000)
+
+print(instance.get('string'))
